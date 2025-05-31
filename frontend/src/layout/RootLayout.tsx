a243ea9f9c9af5261149fb968dfb92aad1d2e194
anderson-wq <andersonsimon2025@gmail.com>
2025-05-31 05:54:36 -0700

    'main'

--- frontend/src/layout/RootLayout.tsx
+++ frontend/src/layout/RootLayout.tsx
@@ -7,13 +7,13 @@ const RootLayout = () => {
   return (
     <div className="flex flex-col md:flex-row md:min-h-screen">
       {/* Sidebar */}
-      <aside className="w-full h-2 border-b border-gray-200 bg-white md:w-16 md:h-auto md:border-b-0 md:border-r">
+      <aside className="w-full h-0 border-b   md:w-16 md:h-auto md:border-b-0 md:border-r">
         <Navbar />
       </aside>
 
       {/* Main content */}
       <main
-        className={`flex-1 md:py-5  overflow-y-auto ${
+        className={`flex-1 md:py-5 py-5  overflow-y-auto ${
           theme === 'light'
             ? 'bg-[#02111B] text-[#00CFC1] shadow-[0_4px_10px_#00CFC1]'
             : 'bg-white text-black'
